e711b2322e8d2dcfb71124d03fd79e85fad0eed7
AmbyMbayi <ambymbayi54@gmail.com>
2020-10-14 14:19:27 +0300

    navbar completed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/styles.scss";
-import logo from "../../assets/logo.png";
+import Logo from "../../assets/logo.png";
+import Profile from "../../assets/profile.jpg";
 
 export default function Navbar() {
   return (
@@ -8,24 +9,68 @@ export default function Navbar() {
       <div className="navbar">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link">
               <i className="fa fa-bars"></i>
             </a>
           </li>
           <li className="nav-item">
-            <img src={logo} alt="" className="logo" />
+            <img src={Logo} alt="" className="logo" />
             <img src="" alt="" />
           </li>
         </ul>
-        <form>
-          <input type="text" placeholder="what are you looking for..." />
+        <form className="navbar-search">
+          <input
+            type="text"
+            placeholder="what are you looking for..."
+            className="navbar-search-input"
+          />
+          <i className="fa fa-search"></i>
         </form>
-        <ul className="navbar-nav">
-          <li>
-            <a href="">link one</a>
+        <ul className="navbar-nav nav-right">
+          <li className="nav-item dropdown">
+            <a className="nav-link">
+              <i className="fa fa-bell"></i>
+              <span className="navbar-badge">15</span>
+            </a>
+            <ul className="dropdown-menu notification-menu">
+              <div className="dropdown-menu-header">
+                <span>Notification</span>
+              </div>
+              <div className="dropdown-menu-content">
+                <li className="dropdown-menu-item">
+                  <a href="" className="dropdown-menu-link">
+                    <div>
+                      <i className="fa fa-gift"></i>
+                    </div>
+                    <span>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Voluptate aut facere expedita atque ex laudantium alias
+                      nesciunt, similique ab ea molestiae ratione id consectetur
+                      cumque facilis nemo. Natus, quibusdam vel. <br />
+                      <span>15/07/2020</span>
+                    </span>
+                  </a>
+                </li>
+              </div>
+              <div className="dropdown-menu-footer">
+                <span>View all notification</span>
+              </div>
+            </ul>
           </li>
-          <li>
-            <a href="">link two</a>
+          <li className="nav-item avt-wrapper">
+            <div className="avt dropdown">
+              <img src={Profile} alt="" />
+              <ul className="dropdown-menu">
+                <li className="dropdown-menu-item">
+                  <a className="dropdown-menu-link">
+                    <div>
+                      <i className="fa fa-user"></i>
+                    </div>
+                    <span>Profile</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
